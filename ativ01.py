0795a34f329750285f1de0a21b44eb2d1e75a38a
natan1144 <natanhpena@gmail.com>
2024-08-27 10:40:13 -0300

    atividades 1 e 2

---
+++ ativ01.py
@@ -0,0 +1,8 @@
+# solicitando e recebendo os numeros para a soma
+num1 = input("Digite um numero: ")
+num1 =float(num1)
+num2 = input("Digite outro numero: ")
+num2 =float(num2)
+
+#exibindo e fazendo a soma dos numeros recebidos
+print(f"A soma dos numeros sao: {num1 + num2}")
---
+++ ativ02.py
@@ -0,0 +1,8 @@
+# solicitando e recebendo os numeros para a subtraçao
+num1 = input("Digite um numero: ")
+num1 =float(num1)
+num2 = input("Digite outro numero: ")
+num2 =float(num2)
+
+
+print(f"A subtraçao dos numeros sao: {num1 - num2}")
