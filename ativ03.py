1f30eedfb161fbdd231a2699cc3f3c6311a09296
jeanpietro <jeanpietrokeylle13@gmail.com>
2024-08-27 10:52:35 -0300

    Atividades 3 e 4

---
+++ ativ03.py
@@ -0,0 +1,8 @@
+# Solicitando e recebendo os numeros para calcular o produto
+num1 = input('Digite um número: ')
+num1 = float(num1)
+num2= input('Digite um número: ')
+num2 = float(num2)
+
+#exibindo e fazendo o resultado dos numeros recebidos
+print(f'O produto dos dois números são: {num1 * num2}')
---
+++ ativ04.py
@@ -0,0 +1,9 @@
+# Solicitando e recebendo os numeros para calcular a divisão
+num1 = input('Digite um número: ')
+num1 = float(num1)
+num2= input('Digite um número: ')
+num2 = float(num2)
+
+# exibindo e fazendo a divisão dos numeros recebidos
+
+print(f'O resultado da divisão é: {num1 / num2}')
